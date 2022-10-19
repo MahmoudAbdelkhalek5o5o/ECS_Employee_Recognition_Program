72ff92cdddd8ad24a6f1fd78b21fad2f7d05e6a2
mohamed3610 <muhammad.mazen4@gmail.com>
2022-10-19 09:32:54 +0200

    push

--- Employee_Recognission_Program/activities/admin.py
+++ Employee_Recognission_Program/activities/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
+from .models import CategoryArchive , Activity ,ActivitySuggestion , ActivityCategory , ActivityArchive , ActivityRequest , ActivityRestorationRequest , OldActivityRequest , OldDataActivities , OldDataActivitySuggestion , OldDataCategory
 # Register your models here.
+
+@admin.register(ActivityCategory)
+class ViewAdmin(ImportExportModelAdmin):
+        pass
+
+
+
+@admin.register(Activity)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
+
+admin.site.register(ActivityArchive)
+admin.site.register(CategoryArchive)
+admin.site.register(ActivityRequest)
+admin.site.register(ActivityRestorationRequest)
+admin.site.register(OldActivityRequest)
+admin.site.register(OldDataActivities)
+admin.site.register(OldDataActivitySuggestion)
+admin.site.register(OldDataCategory)
+admin.site.register(ActivitySuggestion)
+
---
+++ Employee_Recognission_Program/activities/resources.py
@@ -0,0 +1,4 @@
+from import_export import resources
+from .models import User
+from django.utils.translation import gettext_lazy as _
+
