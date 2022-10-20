ac3a527729f501e6e02814a150cfd8c76d3c4793
MahmoudAbdelkhalek5o5o <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2022-10-20 17:07:23 +0200

    filter archiving

--- Employee_Recognission_Program/activities/admin.py
+++ Employee_Recognission_Program/activities/admin.py
@@ -2,16 +2,34 @@ from django.contrib import admin
 from django.contrib import messages
 
 from import_export.admin import ImportExportModelAdmin
+from numpy import rec
 from .models import CategoryArchive , Activity ,ActivitySuggestion , ActivityCategory , ActivityArchive , ActivityRequest , ActivityRestorationRequest , OldActivityRequest , OldDataActivities , OldDataActivitySuggestion , OldDataCategory
-
+from django.utils.translation import gettext_lazy as _
 from datetime import datetime
-import pytz
 # Register your models here.
 
+class Filter(admin.SimpleListFilter):
+    title = _('Archived')
+    parameter_name = 'Is Archived'
+    def lookups(self, request, model_admin):
+
+        return (
+            ('Yes', _('Yes')),
+            ('No', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'Yes':
+            return queryset.filter(
+                is_archived = True
+            )
+        if self.value() == 'No':
+            return queryset.filter(
+                is_archived = False
+            )
 
 
-
-@admin.action(description='Archive Activity')
+@admin.action(description='Archive Category')
 def AdminArchiveCategory(modeladmin, request, queryset):
     queryset.update(is_archived = True)
     # messages.error(request, f'Activity cannot be restored after the end date of id {cat.id} with the name of {cat.activity_name}')  
@@ -24,10 +42,13 @@ def AdminArchiveCategory(modeladmin, request, queryset):
 # Register your models here.
 
 @admin.register(ActivityCategory)
-class ViewAdmin(ImportExportModelAdmin):
+class ViewAdminCategory(ImportExportModelAdmin):
     actions = [AdminArchiveCategory]
-    list_filter = ('is_archived','end_date')
-
+    list_filter = (Filter,)
+    def get_queryset(self, request):
+        data = super().get_queryset(request)
+        data.filter(end_date__lte=datetime.now()).update(is_archived=True)
+        return data.filter()
 
 
 @admin.register(Activity)
