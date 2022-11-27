3203bcccfa4f4899ef44fcdf5921e0e7303a5498
seifgamal98 <74654269+seifgamal98@users.noreply.github.com>
2022-11-27 17:04:15 +0200

    last

--- Employee_Recognission_Program/Rewards/urls.py
+++ Employee_Recognission_Program/Rewards/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
         path("suggest_vendor", views.suggest_vendor, name = "suggest-vendor"),
+        path("view_vendors", views.view_vendors, name = "view-vendors"),
+        path("view_rewards/<int:vendor_id>", views.view_rewards, name = "view_rewards")
 
 ]
--- Employee_Recognission_Program/Rewards/views.py
+++ Employee_Recognission_Program/Rewards/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+import pytz
+from datetime import datetime,date,timedelta
 from .models import Suggest_vendor ,Vendor , Reward 
 from Users.models import User , ROLE
 # Create your views here.
@@ -20,3 +22,31 @@ def suggest_vendor(request):
     else:
                
         return render(request,"rewards/suggest_vendor.html")
+
+def view_vendors(request):
+    if request.user.is_authenticated:
+        all_vendors = Vendor.objects.filter().all()
+        vendors = []
+        utc=pytz.UTC
+        now = utc.localize(datetime.now())
+        for vendor in all_vendors:
+            if vendor.start_date <= now:
+                vendors.append(vendor)
+        return render(request,"rewards/view_vendors.html",{
+            "vendors": vendors
+        })
+    else:
+        redirect("users-home")
+
+def view_rewards(request,vendor_id):
+    if request.user.is_authenticated:
+        Reward.objects.filter(end_date__lt=date.today()).update(is_archived= True)
+        rewards = Reward.objects.filter(is_archived = False, vendor = vendor_id)
+        rewards1 = Reward.objects.filter(is_archived = False, vendor = vendor_id)[0]
+        print(rewards1)
+        return render(request,"rewards/view_rewards.html",{
+            "rewards":rewards,
+            "rewards1":rewards1
+        })
+    else:
+        return redirect("users-home")
